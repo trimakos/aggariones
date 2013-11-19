656eb49541bd69cf8b896ade73300d718eb97159
Artemis Matsas <aremis_matsas@outlook.com.gr>
2013-11-19 20:03:45 +0200

    Just Created it!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stoixdb::Application.config.secret_key_base = 'a05921072a7a8ca89ec1042dcbafdf4aadc798ddfb98ff2ec5ec538d4409ce0a903736c33321dc34fb9dcb24353300443de624842f77353cd3173e440b563e52'
